36d2ccf9e060dc34e68cc0fbb09225010a4ae19d
fyandun <francisco.yandun@gmail.com>
2023-04-23 18:38:00 -0400

    Added Mark's ee

---
+++ README.md
@@ -0,0 +1,72 @@
+# xArm_Amiga_CMU
+
+# Setup
+Simple. Mainly Gazebo and MoveIt package installation required. Then clone this repository in your `catkin_ws/src` and then `catkin_make`. Additional instructions can be found in the original [xarm_ros repository](https://github.com/xArm-Developer/xarm_ros#3-preparations-before-using-this-package) 
+
+## Create Amiga model in Gazebo
+1. Move model directory to gazebo directory 
+```
+cd ~/.gazebo/models
+mkdir amiga
+cd ~/catkin_ws/src/xArm_Amiga_CMU/xarm_gazebo/models
+cp -dir amiga/ ~/.gazebo/models/
+```
+
+# Running without custom End-Effector
+1. Gazebo 
+```
+roslaunch xarm_gazebo amiga_xarm6.launch
+``` 
+Error: No p gain specified for pid.  `Namespace: /xarm/gazebo_ros_control/pid_gains/joint is` expected and can be ignored. </br>
+
+2. MoveIt & xArm_Planner 
+```
+roslaunch xarm6_moveit_config xarm6_moveit_gazebo_plan.launch
+```
+Manually move the EE in MoveIt GUI and press `Plan & Execute` to move the robot. Both RViz and Gazebo robot arm should move.
+
+3. Python Script for desired Motion using ROS service to MoveGroup Interface
+(if first time, make this python script executable)
+```
+~/catkin_ws/src/xArm_Amiga_CMU/xarm_planner/scripts
+chmod +x xArm_planar_motion.py
+python xArm_planar_motion.py
+```
+
+![Motion from scripted interface](doc/amiga_script.png)
+
+
+# Running with custom End-Effector
+1. Gazebo 
+```
+roslaunch xarm_gazebo amiga_xarm6.launch add_gripper:=true
+``` 
+Error: No p gain specified for pid.  `Namespace: /xarm/gazebo_ros_control/pid_gains/joint is` expected and can be ignored. </br>
+
+2. MoveIt & xArm_Planner 
+```
+roslaunch xarm6_gripper_moveit_config xarm6_gripper_moveit_gazebo.launch
+```
+Wait until the terminal prompts `You can start planning now!` message, which means RViz can now take in manual planning requests.
+Manually move the EE in MoveIt GUI and press `Plan & Execute` to move the robot. Both RViz and Gazebo robot arm should move. Can manually drag the EE to be in the configuration shown below </br>
+
+
+![Motion from scripted interface](doc/EE.png)
+
+
+3. (A) Python Script for desired Motion using ROS service to MoveGroup Interface
+
+<strong>TODO: Currently, this launch setup does not work with the python script `xArm_planar_motion.py` as it did above. Needs to be resolved to work with MoveGroup script interface.</strong> </br>
+
+
+3. (B) Checking MoveIt planning feasibility with custom end-effector. 
+Run this separately from previous MoveIt file. The previous `xarm6_gripper_moveit_gazebo.launch` is planning with a smaller EE and NOT the custom EE. The `demo_customEE_gazebo.launch` file accounts for the right dimension of the gripp.er
+```
+roslaunch xarm6_moveit_config demo_customEE_gazebo.launch 
+```
+
+<strong>TODO: Currently, this MoveIt launch setup does not work with Gazebo to update the joint angles. Needs to be resolved to work with above Gazebo setup and python script. </strong></br>
+
+
+![Motion from scripted interface](doc/EE2.png)
+
---
+++ xarm_planner/scripts/xArm_planar_motion.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+from xarm_planner.srv import *
+from geometry_msgs.msg import Pose
+
+import xArmPlanner_srv2python_wrapper as xArm_srv_wrapper
+
+"""
+Main script to execute that handles xArm motion for Corn Stalk insertion for CMU-ISU 2023 field test. 
+
+Author:
+- Mark Lee (MoonRobotics@cmu.edu)
+
+Date:
+- Apr 2023.
+"""
+
+HOME_POSE = [0.207, 0.0, 0.112, 0., 0., 0., 1.] 
+HOME_POSE_WITH_ROT = [0.207, 0.0, 0.112, 0.706675, 0., 0., 0.70754]
+MID_PT_POSE_TO_APPROACH = [0.293, 0.0, 0.67, 0.706675, 0., 0., 0.70754]
+APPROACH_POSE = [0.293, 0.05, 0.67, 0.706675, 0., 0., 0.70754]
+INSERT_POSE = [0.293, -0.25, 0.67, 0.706675, 0., 0., 0.70754]
+
+
+def get_desired_pose_from_list(input_list):
+
+    x = input_list[0]
+    y = input_list[1]
+    z = input_list[2]
+    qx = input_list[3]
+    qy = input_list[4]
+    qz = input_list[5]
+    qw = input_list[6]
+
+    return x, y, z, qx, qy, qz, qw
+
+def corn_stalk_motion():
+    #default home pose
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(HOME_POSE)
+
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: GO_HOME_POSE, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: GO_HOME_POSE, Completed True/False?: {request_states}")
+
+    #default home pose w rotation
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(HOME_POSE_WITH_ROT)
+
+    
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: HOME_POSE_WITH_ROT, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: HOME_POSE_WITH_ROT, Completed True/False?: {request_states}")
+
+    #Mid pt to approach pose for higher planning success rate
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(MID_PT_POSE_TO_APPROACH)
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: MID_PT_POSE_TO_APPROACH, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: MID_PT_POSE_TO_APPROACH, Completed True/False?: {request_states}")
+
+    #Approach pose before inserting sensor into corn stalk
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(APPROACH_POSE)
+    
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
+
+    #pose for inserting sensor into corn stalk
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(INSERT_POSE)
+   
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: INSERT_CARTESIAN, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: INSERT_CARTESIAN, Completed True/False?: {request_states}")
+
+if __name__ == "__main__":
+    print(" ================ start ============ ")
+
+    #obtained from manually playing with MoveIt and then reading joint postions
+    # STOW_JOINT_ANGLES = [-0.005691000155291448, -1.5617350979917042, -3.817223679725146e-05, -0.0002003826846523893, -0.00011472079979313321, 3.616551335650087e-05]
+    # APPROACH_JOINT_ANGLES = [-0.02991102667951928, -0.311088608000313, -1.189797271512119, 1.6267551770805806, 1.47070589650495, -0.0957004890775206]
+
+    #create wrapper object
+    xArm_srv_wrapper = xArm_srv_wrapper.Srv2Python()
+
+    #execute scripted motion for corn stalk insertion
+    corn_stalk_motion()
+    print(" ================ completed script ============ ")
+
+
+    
+
+
